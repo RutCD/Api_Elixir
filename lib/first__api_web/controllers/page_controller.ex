dbfe703c74427a8cf33925895e500967c29239af
RutCD <rutchulin0@gmail.com>
2022-02-07 15:38:49 -0500

    feature: create modules

--- lib/first__api_web/controllers/page_controller.ex
+++ lib/first__api_web/controllers/page_controller.ex
@@ -4,4 +4,30 @@ defmodule FirstApiWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def weather do
+
+    request("https://api.openweathermap.org/data/2.5/weather?q=playa%20del%20carmen&APPID=d9d025affffb89c75f8653c84b044b40")
+    |> body
+    |> message
+    |> show
+  end
+
+  def request(url) do
+    HTTPotion.get url
+  end
+
+  def body(response) do
+    response.body
+  end
+
+  def message(map) do
+    "> #{map["name"]} tiene una visibilidad de #{map["visibility"]}"
+  end
+
+  def show(message) do
+    IO.puts message
+  end
+
+
 end
